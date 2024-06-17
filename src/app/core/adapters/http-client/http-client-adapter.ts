aa41faa9c586021021fa95896e67b4a99e262f2d
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-06-17 11:54:31 -0300

    Atualizado angular cdk

--- src/app/core/adapters/http-client/http-client-adapter.ts
+++ src/app/core/adapters/http-client/http-client-adapter.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 
 import { HttpClientMock } from '@assets/mocks/http-client-mock';
 
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, HttpClientModule],
-  providers: [
-    provideHttpClient(withFetch()),
-    {
-      provide: HttpClient,
-      useClass: HttpClientMock
-    }
-  ]
-})
+@NgModule({ declarations: [], imports: [CommonModule], providers: [
+        provideHttpClient(
+            withFetch(),
+            withInterceptorsFromDi()
+        ),
+        {
+            provide: HttpClient,
+            useClass: HttpClientMock
+        },
+    ] })
 export class HttpClientAdapterModule { }
