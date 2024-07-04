99eecc59f527dfd5404a949651223df9268272f7
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-07-04 17:48:05 -0300

    Criação de tela de recuperação de senha

--- src/app/features/recovery-password/recovery-password.component.ts
+++ src/app/features/recovery-password/recovery-password.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { RouteEnum } from '@enums/routes/route.enum';
+import { RecoveryPasswordFormEnum } from '@enums/forms/recovery-password-form';
+import { RecoveryPasswordForm } from './models/recovery-password-form.interface';
 
 @Component({
   selector: 'app-recovery-password',
@@ -15,18 +17,27 @@ import { RouteEnum } from '@enums/routes/route.enum';
   templateUrl: './recovery-password.component.html',
   styleUrl: './recovery-password.component.scss'
 })
-export class RecoveryPasswordComponent {
+export class RecoveryPasswordComponent implements OnInit {
+  protected recoveryPasswordFormGroup: FormGroup<RecoveryPasswordForm>;
   protected readonly RouteEnum = RouteEnum;
-  protected readonly recoveryPasswordFormGroup = new FormGroup({
-    email: new FormControl(null, [
-      Validators.required,
-      Validators.email
-    ])
-  });
+  protected readonly RecoveryPasswordFormEnum = RecoveryPasswordFormEnum;
 
   constructor(private readonly router: Router) {}
 
+  ngOnInit(): void {
+    this.createFormGroup();
+  }
+
   goTo(path: RouteEnum): void {
     this.router.navigate([path]);
+  }
+
+  private createFormGroup(): void {
+    this.recoveryPasswordFormGroup = new FormGroup<RecoveryPasswordForm>({
+      [RecoveryPasswordFormEnum.EMAIL]: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ])
+    });
   }
 }
